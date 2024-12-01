decabf6d39a69cf8c96eedbf0cfbf36656a2fc57
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2024-12-01 12:50:29 -0800

    Add trivial state handling

--- src/useWebSocket.ts
+++ src/useWebSocket.ts
@@ -1,9 +1,39 @@
 import {useEffect, useState} from 'react';
 
+interface Aircraft {
+  army: number;
+  id: string;
+  x: number;
+  y: number;
+  z: number;
+}
+
+interface Army {
+  id: number;
+  countries: string;
+  name: string;
+}
+
+export interface WorldStateItem {
+  aircrafts: Aircraft[];
+  armies: Army[];
+  battle_area: {
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+    sectory_size: number;
+  };
+  map_Name: string;
+  // FIXME: Can this annotation be improved?
+  mission_time: string;
+}
+
 type ConnectionState = 'disconnected' | 'connecting' | 'connected';
 
 export default function useWebSocket(port = 8020) {
   const [connected, setConnected] = useState<ConnectionState>('disconnected');
+  const [states, setStates] = useState<WorldStateItem[]>([]);
   useEffect(() => {
     const socket = new WebSocket(`ws://localhost:${port}`);
     setConnected('connecting');
@@ -13,7 +43,19 @@ export default function useWebSocket(port = 8020) {
       console.log('useWebSocket.open', event);
     });
     socket.addEventListener('message', (event) => {
-      console.log('useWebSocket.message', event);
+      try {
+        const state: WorldStateItem = JSON.parse(event.data);
+        setStates((previousStates) => {
+          const states = [...previousStates];
+          states.push(state);
+          if (states.length > 20) {
+            states.shift();
+          }
+          return states;
+        });
+      } catch (e) {
+        console.error(e);
+      }
     });
     socket.addEventListener('error', (event) => {
       console.log('useWebSocket.error', event);
@@ -26,5 +68,5 @@ export default function useWebSocket(port = 8020) {
       socket.close();
     };
   }, [port]);
-  return {connected};
+  return {connected, states};
 }
