7f0bdb26818d9a7dfb1928b3a3ff63e9275bcd42
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2024-12-01 12:30:11 -0800

    Actually send updates down, 1 per second

--- server/websocket.ts
+++ server/websocket.ts
@@ -1,4 +1,6 @@
 import {WebSocketServer, type WebSocket} from 'ws';
+// @ts-expect-error bun doesn't care about this, lol
+import updates from './updates.json';
 
 const PORT = 8020;
 
@@ -9,10 +11,15 @@ console.log(`WebSocket server is running on ws://localhost:${PORT}`);
 server.on('connection', (socket: WebSocket) => {
   console.log('WebSocket: Client Connected');
 
+  let index = 0;
+  const interval = setInterval(() => {
+    const update = updates[index];
+    socket.send(JSON.stringify(update));
+    index++;
+  }, 1000);
+
   socket.on('close', () => {
     console.log('WebSocket: Client Disconnected');
+    clearInterval(interval);
   });
-
-  // Actually implement this
-  socket.send('Hello!');
 });
